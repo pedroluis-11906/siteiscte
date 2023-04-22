95a24526fa787195bc796180084ca4800a804e17
Pedro Luis <pmaal1@iscte-iul.pt>
2023-04-22 13:54:15 +0100

    entrega semana8 até ao EX5

--- votacao/static/votacao/js/scripts.js
+++ votacao/static/votacao/js/scripts.js
@@ -10,19 +10,61 @@ $(document).ready( function (){
     });
 })
 
+$(document).ready(
+    function (){
+        $('#showprofile .profileimg').hover(
+            function (){
+                $('#user-info').show();
+            },
+            function (){
+                $('#user-info').hide();
+            }
+            )
+})
+
+
+
 function esconder_mostrar(){
     let list = document.getElementById('question-list');
     let btn = document.getElementById('hide-show-quest');
-    if (list.style.display == 'none'){
+    if (list.style.display === 'none'){
         list.style.display = 'block';
         btn.textContent = 'Esconder lista de questões';
     } else {
         list.style.display = 'none';
-        btn = 'Mostrar lista de questões';
+        btn.textContent = 'Mostrar lista de questões';
     }
 }
 
-function validate(){
+function validateComment() {
+    let comment = document.getElementById('comment');
+    let commentValue = comment.value;
+    let result = document.getElementById('comment-result');
+    const badWords = ["abécula", "abentesma", "achavascado", "estólido", "pechenga", "somítico"];
+    let hasBadWords = false;
 
+    if (commentValue !== "") {
+        console.log('not null');
+
+        for (let i = 0; i <= badWords.length; i++) {
+            if (commentValue.includes(badWords[i])) {
+                hasBadWords = true;
+                console.log('true');
+                break;
+            }
+        }
+
+        if (hasBadWords) {
+            result.textContent = 'Comentário não aceite.';
+            result.style.display = "block";
+            comment.value = '';
+        } else {
+            result.textContent = 'Comentário aceite.';
+            result.style.display = "block";
+        }
+
+    }
 }
+
+
 
